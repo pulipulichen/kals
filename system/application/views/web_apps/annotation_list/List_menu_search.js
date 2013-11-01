f219d58d72711c09184e80be992cfdabfbb3aeea
Pulipuli Chen <pulipuli.chen@gmail.com>
2013-11-02 06:15:46 +0800

    補充幾個檔案

--- system/application/views/web_apps/annotation_list/List_menu_search.js
+++ system/application/views/web_apps/annotation_list/List_menu_search.js
@@ -1,5 +1,5 @@
 /**
- * List_menu_block
+ * List_menu_search
  *
  * @package    KALS
  * @category   Webpage Application Libraries
@@ -12,29 +12,29 @@
  * @param {List_item} _item
  * @param {String[]} _disable_option
  */
-function List_menu_block(_item, _disable_option) {
+function List_menu_search(_item, _disable_option) {
     
     List_menu.call(this, _item, _disable_option);
     
 }
 
-List_menu_block.prototype = new List_menu();
+List_menu_search.prototype = new List_menu();
 
 /**
  * 顯示完整的日期
  */
-List_menu_block.prototype.timestamp_full_dispaly = true;
+List_menu_search.prototype.timestamp_full_dispaly = true;
 
 /**
  * Create UI
- * @memberOf {List_menu_block}
+ * @memberOf {List_menu_search}
  * @type {jQuery} UI
  */
-List_menu_block.prototype._$create_ui = function () {
+List_menu_search.prototype._$create_ui = function () {
     var _ui = List_menu.prototype._$create_ui.call(this);
     _ui.addClass('list-menu-block');
     return _ui;
 };
 
-/* End of file List_menu_block */
-/* Location: ./system/application/views/web_apps/List_menu_block.js */
+/* End of file List_menu_search */
+/* Location: ./system/application/views/web_apps/List_menu_search.js */
