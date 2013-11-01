ab8a710d3343abafaf6865c2c821bb956c3b4d08
Pulipuli Chen <pulipuli.chen@gmail.com>
2013-11-01 18:52:14 +0800

    增加更新指令

---
+++ update_kals.sh
@@ -0,0 +1,6 @@
+BRANCH=reading-map
+
+cd /opt/lampp/htdocs/kals
+git --git-dir=/opt/lampp/htdocs/kals/.git clean -f -d
+git --git-dir=/opt/lampp/htdocs/kals/.git reset --hard origin/$BRANCH
+git --git-dir=/opt/lampp/htdocs/kals/.git pull
