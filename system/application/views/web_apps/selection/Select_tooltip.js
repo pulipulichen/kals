68032247711506ab39a66cb84faa34e4172341b1
Pulipuli Chen <pulipuli.chen@gmail.com>
2013-11-08 03:22:19 +0800

    調整過Select_tooltip下的位置，新增特例

--- system/application/views/web_apps/selection/Select_tooltip.js
+++ system/application/views/web_apps/selection/Select_tooltip.js
@@ -81,29 +81,52 @@ Select_tooltip.prototype._$get_config = function () {
         if (_tip.length == 0)
             return;
             
-        if (_select_tooltip.enable_select == false)
+        if (_select_tooltip.enable_select == false) {
             return;
+        }
         
         var _trigger = _this.getTrigger();
+                
         $('.tooltip-trigger-hover').removeClass('tooltip-trigger-hover');
         _trigger.addClass('tooltip-trigger-hover');
         
         //$.test_msg('Select_tooltip._$get_config()', _tip.length);
         
+        /**
+         * 20131108 Pulipuli Chen
+         * 只有在pdf2htmlEX的特殊情況下才使用的定位
+         */
+        if ($(".ff1").length > 0) {
+            
+            var _trigger_offset = _trigger.offset();
+            _tip_left = (_trigger_offset.left + _trigger.width() / 2 / 2) - (_tip.width() / 2 );
+            _tip.css("visibility", "hidden");
+            setTimeout(function () {
+                _tip.css("left", _tip_left + "px");
+                _tip.css("visibility", "visible");
+            }, 0);
+            
+            /*
+            var _pos = _trigger.offset();
+            _trigger.css("border", "1px solid red");
+            $.test_msg("Select_tooltip", [_trigger.text(), _pos.top, _pos.left, _trigger.attr("offsetWidth"), _trigger.width(), _trigger.outerWidth(false), _trigger.css("width")]);
+            var _tip_pos = _tip.offset();
+            $.test_msg("tooltiop", [_tip_left, _tip_pos.top, _tip_pos.left, _tip.width()]);
+            //return;
+            */
+        }
+        
         //調整tip的位置
         setTimeout(function () {
-            
             var _trigger_bottom;
             var _tip_top = _tip.offset().top;
             var _top_padding = KALS_toolbar.get_ui().height();
-            if (_tip_top < window.pageYOffset + _top_padding)
-            {             
+            if (_tip_top < window.pageYOffset + _top_padding) {             
                _trigger_bottom = _trigger.offset().top + _trigger.height();
                    _tip.css('top', _trigger_bottom+'px');
                _tip.addClass('bottom');
             }
-            else
-            {
+            else {
                 _tip.removeClass('bottom');
             }
             
@@ -113,8 +136,8 @@ Select_tooltip.prototype._$get_config = function () {
             // 如果他沒有對到字的正上方，則調整一下吧
             //$.test_msg('Select_tooltip._$get_config()', [_trigger.offset().left, _trigger.offset().top, _tip.offset().left, _tip.offset().top]);
             var _trigger_offset = _trigger.offset();
-            if (Math.abs( _tip_left - _trigger_offset.left) > 50 )
-            {
+            
+            if (Math.abs( _tip_left - _trigger_offset.left) > 50 ) {
                 //那就定位在滑鼠上方
                 //$.test_msg('Select_tooltip._$get_config()', [_event.clientX, _event.clientY]);
                 _tip_left = _event.clientX - (_tip.width() / 2);
@@ -123,20 +146,18 @@ Select_tooltip.prototype._$get_config = function () {
                 _tip_top = window.pageYOffset + _event.clientY - _tip.height() - 3;
                 //$.test_msg('Select_tooltip._$get_config()', [_tip_top]);
                 _tip.css('top', _tip_top+'px');
-                if (_tip_top < window.pageYOffset + _top_padding)
-                {             
+                if (_tip_top < window.pageYOffset + _top_padding) {             
                    _tip_top = window.pageYOffset + _event.clientY + (_trigger.height() / 3);
                        _tip.css('top', _tip_top+'px');
                    _tip.addClass('bottom');
                    //$.test_msg('Select_tooltip._$get_config() bottom', [_event.clientY, _tip_top, window.pageYOffset, _top_padding]);
                 }
-                else
-                {
+                else {
                     //$.test_msg('Select_tooltip._$get_config() top', [_tip_top]);
                     _tip.css('top', _tip_top+'px');
                     _tip.removeClass('bottom');
                 }
-            }
+            }   //if (Math.abs( _tip_left - _trigger_offset.left) > 50 ) {
             
             /*
             if (_tip_left < _x_left + 30)
@@ -183,13 +204,14 @@ Select_tooltip.prototype._$get_config = function () {
         
         //在顯示之前，決定是否要調整
         var _selected_classname = 'selected';
-        if (KALS_text.selection.select._select_from != null)
+        if (KALS_text.selection.select._select_from != null) {
             _tip.addClass(_selected_classname);
-        else
+        }
+        else {
             _tip.removeClass(_selected_classname);
+        }
         
-        if ($.is_function(_onbeforeshow))
-        {
+        if ($.is_function(_onbeforeshow)) {
             _onbeforeshow.call(this);
         }
         
@@ -198,8 +220,9 @@ Select_tooltip.prototype._$get_config = function () {
     var _onbeforehide = $.get_parameter( _config, 'onBeforeHide' );
     _config['onBeforeHide'] = function (_this) {
         
-        if (_select_tooltip.enable_select == false)
+        if (_select_tooltip.enable_select == false) {
             return;
+        }
         
         //if ($.is_null(_this))
         //{
@@ -212,13 +235,15 @@ Select_tooltip.prototype._$get_config = function () {
         var _trigger = _this.getTrigger();
         _trigger.removeClass('tooltip-trigger-hover');
         
-        if ($.is_function(_onbeforehide))
+        if ($.is_function(_onbeforehide)) {
             _onbeforehide.call(this);
+        }
         
     };    //onBeforeHide: function () {
     
-    if ($.is_mobile_mode())
+    if ($.is_mobile_mode()) {
         _config['effect'] = 'toggle';
+    }
     //else
     //    _config['effect'] = 'fade';
     
@@ -232,8 +257,9 @@ Select_tooltip.prototype._$get_config = function () {
 Select_tooltip.prototype.tooltip_config = null;
 
 Select_tooltip.prototype.get_tooltip_config = function () {
-    if (this._tooltip_config == null)
+    if (this._tooltip_config == null) {
         this._tooltip_config = this._$get_config();
+    }
     return this._tooltip_config;
 };
 
