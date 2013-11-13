04e44599e9018166021bbf349f4719d259065ade
Pulipuli Chen <pulipuli.chen@gmail.com>
2013-11-13 16:32:19 +0800

    調整他的功能

--- help/demo/pdf2htmlEX.js
+++ help/demo/pdf2htmlEX.js
@@ -100,11 +100,13 @@ var pdf2htmlEX = (function(){
     },
     /* return if any part of this page is shown in the container */
     is_visible : function() {
+        return true;
       var off = this.position();
       return !((off[1] > this.height()) || (off[1] + this.$container.height() < 0));
     },
     /* return if this page or any neighbor of it is visible */
     is_nearly_visible : function() {
+        retunr true;
       var off = this.position();
       /* I should use the height of the previous page or the next page here
        * but since they are not easily available, just use '*2', which should be a good estimate in most cases
