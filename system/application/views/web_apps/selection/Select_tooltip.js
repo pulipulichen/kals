7f9f638edf5de1f3b60be347ef37c2769717d309
Pulipuli Chen <pulipuli.chen@gmail.com>
2013-11-16 20:17:50 +0800

    Select_tooltip.js被我改壞了，我要還原重改

--- system/application/views/web_apps/selection/Select_tooltip.js
+++ system/application/views/web_apps/selection/Select_tooltip.js
@@ -34,25 +34,24 @@ Select_tooltip.prototype.enable_select = false;
 
 Select_tooltip.prototype.tooltip_id = 'kals_select_tooltip';
 
+/**
+ * 取得設定參數
+ * @type {JSON}
+ */
+Select_tooltip.prototype._config = null;
+
 Select_tooltip.prototype._$get_config = function () {
     
     var _select_tooltip = this;
     
     var _config = Tooltip_modal.prototype._$get_config.call(this, '#' + this.tooltip_id + ':first');
-    //_config['delay'] = 30;
-    //_config['predelay'] = 100;
+	this._config = _config;
     
     if ($.is_mobile_mode())
     {
         _config['events'] = {
             def: 'mouseenter click,null'
         }; 
-    }
-    else
-    {
-        //_config['events'] = {
-        //    def: 'mouseenter click,mouseleave'
-        //};    
     }
     
     if ($.is_touchable())
@@ -71,176 +70,19 @@ Select_tooltip.prototype._$get_config = function () {
        }
     }
     
-    var _onbeforeshow = _config['onBeforeShow'];
+	// ---------------
+	
+	var _this = this;
     _config['onBeforeShow'] = function (_event) {
-        
-        //if ($.is_null(_this))
-            _this = this;
-        
-        var _tip = _this.getTip();
-        if (_tip.length == 0)
-            return;
-            
-        if (_select_tooltip.enable_select == false) {
-            return;
-        }
-        
-        var _trigger = _this.getTrigger();
-                
-        $('.tooltip-trigger-hover').removeClass('tooltip-trigger-hover');
-        _trigger.addClass('tooltip-trigger-hover');
-        
-        //$.test_msg('Select_tooltip._$get_config()', _tip.length);
-        
-        /**
-         * 20131108 Pulipuli Chen
-         * 只有在pdf2htmlEX的特殊情況下才使用的定位
-         */
-        if ($(".ff1").length > 0) {
-            
-            var _trigger_offset = _trigger.offset();
-            _tip_left = (_trigger_offset.left + _trigger.width() / 2 / 2) - (_tip.width() / 2 );
-            _tip.css("visibility", "hidden");
-            setTimeout(function () {
-                _tip.css("left", _tip_left + "px");
-                _tip.css("visibility", "visible");
-            }, 0);
-            
-            /*
-            var _pos = _trigger.offset();
-            _trigger.css("border", "1px solid red");
-            $.test_msg("Select_tooltip", [_trigger.text(), _pos.top, _pos.left, _trigger.attr("offsetWidth"), _trigger.width(), _trigger.outerWidth(false), _trigger.css("width")]);
-            var _tip_pos = _tip.offset();
-            $.test_msg("tooltiop", [_tip_left, _tip_pos.top, _tip_pos.left, _tip.width()]);
-            //return;
-            */
-        }
-        
-        //調整tip的位置
-        setTimeout(function () {
-            var _trigger_bottom;
-            var _tip_top = _tip.offset().top;
-            var _top_padding = KALS_toolbar.get_ui().height();
-            if (_tip_top < window.pageYOffset + _top_padding) {             
-               _trigger_bottom = _trigger.offset().top + _trigger.height();
-                   _tip.css('top', _trigger_bottom+'px');
-               _tip.addClass('bottom');
-            }
-            else {
-                _tip.removeClass('bottom');
-            }
-            
-            var _tip_left = _tip.offset().left;
-            var _x_left = window.pageXOffset;
-            
-            // 如果他沒有對到字的正上方，則調整一下吧
-            //$.test_msg('Select_tooltip._$get_config()', [_trigger.offset().left, _trigger.offset().top, _tip.offset().left, _tip.offset().top]);
-            var _trigger_offset = _trigger.offset();
-            
-            if (Math.abs( _tip_left - _trigger_offset.left) > 50 ) {
-                //那就定位在滑鼠上方
-                //$.test_msg('Select_tooltip._$get_config()', [_event.clientX, _event.clientY]);
-                _tip_left = _event.clientX - (_tip.width() / 2);
-                _tip.css('left', _tip_left + 'px');
-                
-                _tip_top = window.pageYOffset + _event.clientY - _tip.height() - 3;
-                //$.test_msg('Select_tooltip._$get_config()', [_tip_top]);
-                _tip.css('top', _tip_top+'px');
-                if (_tip_top < window.pageYOffset + _top_padding) {             
-                   _tip_top = window.pageYOffset + _event.clientY + (_trigger.height() / 3);
-                       _tip.css('top', _tip_top+'px');
-                   _tip.addClass('bottom');
-                   //$.test_msg('Select_tooltip._$get_config() bottom', [_event.clientY, _tip_top, window.pageYOffset, _top_padding]);
-                }
-                else {
-                    //$.test_msg('Select_tooltip._$get_config() top', [_tip_top]);
-                    _tip.css('top', _tip_top+'px');
-                    _tip.removeClass('bottom');
-                }
-            }   //if (Math.abs( _tip_left - _trigger_offset.left) > 50 ) {
-            
-            /*
-            if (_tip_left < _x_left + 30)
-            {
-                _x_left = _x_left - 30;
-                if (_x_left > 0)
-                {
-                    setTimeout(function () {
-                        if (_tip.offset().left == _tip_left)
-                        {
-                            $.scroll_to({x: _x_left});
-                        }
-                    }, 1000);
-                }
-            }
-            
-            var _x_right = _x_left + $.get_viewport_width();
-            var _tip_right = _tip_left + _tip.width();
-            if (_tip_right > _x_right - 30)
-            {
-                _x_left = _x_left + 30;
-                
-                setTimeout(function () {
-                    if (_tip.offset().left == _tip_left)
-                    {
-                        $.scroll_to({x: _x_left});
-                    }
-                }, 1000);
-            }
-            */
-            
-        }, 0);    //setTimeout(function () {
-        
-        // --------
-        
-        //if ($.is_null(_this))
-        //    _this = this;
-        
-        //var _tip = _this.getTip();
-        //var _trigger = _this.getTrigger();
-        //設定調整            
-        var _id = $.get_prefixed_id(_trigger);
-        _tip.attr('word_id', _id);
-        
-        //在顯示之前，決定是否要調整
-        var _selected_classname = 'selected';
-        if (KALS_text.selection.select._select_from != null) {
-            _tip.addClass(_selected_classname);
-        }
-        else {
-            _tip.removeClass(_selected_classname);
-        }
-        
-        if ($.is_function(_onbeforeshow)) {
-            _onbeforeshow.call(this);
-        }
-        
+        _this.on_before_show(this, _event);
     };    //onBeforeShow: function () {
     
-    var _onbeforehide = $.get_parameter( _config, 'onBeforeHide' );
-    _config['onBeforeHide'] = function (_this) {
-        
-        if (_select_tooltip.enable_select == false) {
-            return;
-        }
-        
-        //if ($.is_null(_this))
-        //{
-            _this = this;
-        //}
-        
-        if (typeof(_this.getTrigger) != 'function'
-            && typeof(this.getTrigger) == 'function')
-            _this.getTrigger = this.getTrigger;
-        var _trigger = _this.getTrigger();
-        _trigger.removeClass('tooltip-trigger-hover');
-        
-        if ($.is_function(_onbeforehide)) {
-            _onbeforehide.call(this);
-        }
-        
+    _config['onBeforeHide'] = function (_event) {
+        _this.on_before_hide(this, _event);
     };    //onBeforeHide: function () {
     
+	// ---------------
+	
     if ($.is_mobile_mode()) {
         _config['effect'] = 'toggle';
     }
@@ -248,6 +90,187 @@ Select_tooltip.prototype._$get_config = function () {
     //    _config['effect'] = 'fade';
     
     return _config;
+};
+
+/**
+ * 設定顯示前的動作
+ */
+Select_tooltip.prototype.on_before_show = function(_this, _event){
+	
+    //if ($.is_null(_this))
+    //var _this = this;
+        
+    if (this.enable_select === false) {
+        return;
+    }
+    
+    var _tip = _this.getTip();
+    if (_tip.length === 0) {
+        return;
+    }
+    
+    var _trigger = _this.getTrigger();
+            
+    $('.tooltip-trigger-hover').removeClass('tooltip-trigger-hover');
+    _trigger.addClass('tooltip-trigger-hover');
+    
+    //$.test_msg('Select_tooltip._$get_config()', _tip.length);
+    
+    /**
+     * 20131108 Pulipuli Chen
+     * 只有在pdf2htmlEX的特殊情況下才使用的定位
+     */
+    if ($(".ff1").length > 0) {
+        
+        var _trigger_offset = _trigger.offset();
+        _tip_left = (_trigger_offset.left + _trigger.width() / 2 / 2) - (_tip.width() / 2 );
+        _tip.css("visibility", "hidden");
+        setTimeout(function () {
+            _tip.css("left", _tip_left + "px");
+            _tip.css("visibility", "visible");
+        }, 0);
+        
+        /*
+        var _pos = _trigger.offset();
+        _trigger.css("border", "1px solid red");
+        $.test_msg("Select_tooltip", [_trigger.text(), _pos.top, _pos.left, _trigger.attr("offsetWidth"), _trigger.width(), _trigger.outerWidth(false), _trigger.css("width")]);
+        var _tip_pos = _tip.offset();
+        $.test_msg("tooltiop", [_tip_left, _tip_pos.top, _tip_pos.left, _tip.width()]);
+        //return;
+        */
+    }
+    
+    //調整tip的位置
+    setTimeout(function () {
+        var _trigger_bottom;
+        var _tip_top = _tip.offset().top;
+        var _top_padding = KALS_toolbar.get_ui().height();
+        if (_tip_top < window.pageYOffset + _top_padding) {             
+           _trigger_bottom = _trigger.offset().top + _trigger.height();
+               _tip.css('top', _trigger_bottom+'px');
+           _tip.addClass('bottom');
+        }
+        else {
+            _tip.removeClass('bottom');
+        }
+        
+        var _tip_left = _tip.offset().left;
+        var _x_left = window.pageXOffset;
+        
+        // 如果他沒有對到字的正上方，則調整一下吧
+        //$.test_msg('Select_tooltip._$get_config()', [_trigger.offset().left, _trigger.offset().top, _tip.offset().left, _tip.offset().top]);
+        var _trigger_offset = _trigger.offset();
+        
+        if (Math.abs( _tip_left - _trigger_offset.left) > 50 ) {
+            //那就定位在滑鼠上方
+            //$.test_msg('Select_tooltip._$get_config()', [_event.clientX, _event.clientY]);
+            _tip_left = _event.clientX - (_tip.width() / 2);
+            _tip.css('left', _tip_left + 'px');
+            
+            _tip_top = window.pageYOffset + _event.clientY - _tip.height() - 3;
+            //$.test_msg('Select_tooltip._$get_config()', [_tip_top]);
+            _tip.css('top', _tip_top+'px');
+            if (_tip_top < window.pageYOffset + _top_padding) {             
+               _tip_top = window.pageYOffset + _event.clientY + (_trigger.height() / 3);
+                   _tip.css('top', _tip_top+'px');
+               _tip.addClass('bottom');
+               //$.test_msg('Select_tooltip._$get_config() bottom', [_event.clientY, _tip_top, window.pageYOffset, _top_padding]);
+            }
+            else {
+                //$.test_msg('Select_tooltip._$get_config() top', [_tip_top]);
+                _tip.css('top', _tip_top+'px');
+                _tip.removeClass('bottom');
+            }
+        }   //if (Math.abs( _tip_left - _trigger_offset.left) > 50 ) {
+        
+        /*
+        if (_tip_left < _x_left + 30)
+        {
+            _x_left = _x_left - 30;
+            if (_x_left > 0)
+            {
+                setTimeout(function () {
+                    if (_tip.offset().left == _tip_left)
+                    {
+                        $.scroll_to({x: _x_left});
+                    }
+                }, 1000);
+            }
+        }
+        
+        var _x_right = _x_left + $.get_viewport_width();
+        var _tip_right = _tip_left + _tip.width();
+        if (_tip_right > _x_right - 30)
+        {
+            _x_left = _x_left + 30;
+            
+            setTimeout(function () {
+                if (_tip.offset().left == _tip_left)
+                {
+                    $.scroll_to({x: _x_left});
+                }
+            }, 1000);
+        }
+        */
+        
+    }, 0);    //setTimeout(function () {
+    
+    // --------
+    
+    //if ($.is_null(_this))
+    //    _this = this;
+    
+    //var _tip = _this.getTip();
+    //var _trigger = _this.getTrigger();
+    //設定調整            
+    var _id = $.get_prefixed_id(_trigger);
+    _tip.attr('word_id', _id);
+    
+    //在顯示之前，決定是否要調整
+    var _selected_classname = 'selected';
+    if (KALS_text.selection.select._select_from !== null) {
+        _tip.addClass(_selected_classname);
+    }
+    else {
+        _tip.removeClass(_selected_classname);
+    }
+	
+	// ----------------
+	
+    //var _config = Tooltip_modal.prototype._$get_config.call(this, '#' + this.tooltip_id + ':first');
+    var _onbeforeshow = $.get_parameter( this._config, 'onBeforeShow' );
+	
+    if ($.is_function(_onbeforeshow)) {
+        _onbeforeshow.call(this);
+    }        
+};
+
+/**
+ * 設定隱藏前的動作
+ */
+Select_tooltip.prototype.on_before_hide = function (_this, _event) {
+	
+    if (this.enable_select === false) {
+        return;
+    }
+    
+    //var _this = this;
+    
+    if (typeof(_this.getTrigger) != 'function'
+        && typeof(this.getTrigger) == 'function') {
+			_this.getTrigger = this.getTrigger;
+	}
+        
+    var _trigger = _this.getTrigger();
+    _trigger.removeClass('tooltip-trigger-hover');
+    
+	// ----------------
+	
+	//var _config = Tooltip_modal.prototype._$get_config.call(this, '#' + this.tooltip_id + ':first');
+    var _onbeforehide = $.get_parameter( this._config, 'onBeforeHide' );
+    if ($.is_function(_onbeforehide)) {
+        _onbeforehide.call(this);
+    }
 };
 
 /**
