492956dbcb818da28021a14ef9e6dacb931480c3
Ting Ting <yuntlu@gmail.com>
2014-04-09 17:13:18 +0800

    0409 有一種會出錯的預感....!?

---
+++ system/application/views/web_apps/selectable_text/Selectable_text_anchor.js
@@ -0,0 +1,250 @@
+/**
+ * Selectable_text_anchor
+ *
+ * 2014/1/2 檢查完成
+ * @package     KALS
+ * @category    Webpage Application Libraries
+ * @author      Pulipuli Chen <pulipuli.chen@gmail.com>
+ * @copyright   Copyright (c) Expression year is undefined on line 7, column 33 in Templates/KALS/KALS_JavaScript_Class.js., Pudding Chen
+ * @license     http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link        https://github.com/pulipulichen/kals
+ * @version     1.0 2014/1/2 下午 08:40:23
+ */
+
+/**
+ * @memberOf {Selectable_text_anchor}
+ * @extends {KALS_user_interface}
+ * @constructor
+ * @param {Selectable_text} _selectable_text 父物件
+ */
+function Selectable_text_anchor(_selectable_text) {
+    
+    this._selectable_text = _selectable_text;
+    this._selectable_text_word = _selectable_text.word;
+    return this;
+    
+}
+
+/**
+ * Extends from KALS_user_interface.
+ * @memberOf {Selectable_text_anchor}
+ */
+Selectable_text_anchor.prototype = new KALS_user_interface();
+
+/**
+ * 父物件
+ * @type {Selectable_text}
+ */
+Selectable_text_anchor.prototype._selectable_text;
+
+/**
+ * 文字相關的物件
+ * @type {Selectable_text_word}
+ */
+Selectable_text_anchor.prototype._selectable_text_word;
+
+// -----------------------------------
+// 內部參數設定
+// -----------------------------------
+
+// -----------------------------------
+// 方法
+// -----------------------------------
+
+/**
+ * 取得該範圍的文字
+ * 
+ * 2014/1/2 21:55 檢查完畢
+ * 22:00 轉接完畢
+ * @param {Scope_collection_param} _scope_coll
+ * @type {String}
+ */
+Selectable_text_anchor.prototype.get_anchor_text = function (_scope_coll) {
+    
+    var _selectable_text_word = this._selectable_text_word;
+    
+    if ($.is_null(_scope_coll)) {
+        return null;
+    }
+    
+    var _anchor_text = '';
+    
+    for (var _i = 0; _i < _scope_coll.length(); _i++) {
+        var _sentence = '';
+        
+        var _scope = _scope_coll.get(_i);
+        var _from = _scope.get_from();
+        var _to = _scope.get_to();
+        
+        for (var _j = _from; _j < _to + 1; _j++) {
+            var _index = _j;
+            var _word = _selectable_text_word.get_word_by_index(_index);
+            var _text = _word.text();
+            		
+            _sentence = _sentence + _text;
+            
+            if (_j < _to
+                && _selectable_text_word.is_word_next_span(_word)) {
+                _sentence = _sentence + ' ';
+            }
+            
+            // @author Pulipuli Chen <pulipuli.chen@gmail.com> 20131230
+            // 如果這裡面很多字的話，表示這是一個英文，應該加上空格
+            if (_j < _to
+                && _selectable_text_word.is_word_next_english(_word)) {
+                _sentence = _sentence + ' ';
+                
+                //$.test_msg("is_word_next_english", _sentence);
+            }
+        }
+        
+        _sentence = $.trim(_sentence);
+        
+        //不同範圍之間，以空格斷句！
+        if (_i > 0) {
+            _anchor_text = _anchor_text + ' ';
+        }
+           
+        _anchor_text = _anchor_text + _sentence;
+    }
+    
+    _anchor_text = $.trim(_anchor_text);
+    
+    //把' "轉換掉
+    //_anchor_text = $.str_replace("'", " ", _anchor_text);
+    //_text= $.str_replace("'", "&amp;", _text);
+	
+    return _anchor_text;
+};
+
+/**
+ * 取得部份的標註範圍文字
+ * 
+ * 21:56 檢查完畢
+ * 22:00 轉接完畢
+ * @param {Scope_collection_param} _scope_coll 要選取的範圍
+ * @param {Number} _max_length 最長字數，預設是50個字。低於這個數字以下不省略
+ * @return {String}
+ */
+Selectable_text_anchor.prototype.get_abbreviated_anchor_text = function (_scope_coll, _max_length) {
+    
+    if (_max_length === undefined) {
+        _max_length = 50;
+    }
+    
+    var _text = this.get_anchor_text(_scope_coll);
+
+    if (_text.length > _max_length) {
+        var _half = parseInt(_max_length / 2);
+
+        var _first_part = _text.substr(0, _half);
+        var _last_part = _text.substr(_text.length - _half, _half);
+        _text = _first_part + "..." + _last_part; 
+    }
+    return _text;
+};
+
+/**
+ * 取得該範圍的文字
+ * @param {Scope_collection_param} _scope_coll
+ * @param {Scope_collection_param} _focus_coll
+ */
+Selectable_text_anchor.prototype.get_display_anchor_text = function (_scope_coll, _focus_coll) {
+    
+    if ($.is_null(_scope_coll)) {
+        return null;
+    }
+    
+    var _anchor_text = $('<span></span>');
+    
+    var _focus_index = [];
+    var _focus_head_index = [];
+    var _focus_foot_index = [];
+    if ($.is_class(_focus_coll, 'Scope_collection_param')) {
+        _focus_index = _focus_coll.get_index_array();
+        _focus_head_index = _focus_coll.get_from_index_array();
+        _focus_foot_index = _focus_coll.get_to_index_array();
+    }
+    
+    var _focus_text = function (_index, _text) {
+        if ($.inArray(_index, _focus_head_index) > -1) {
+            _text = '<span class="select select_from view">' + _text + '</span>';
+        }
+        else if ($.inArray(_index, _focus_foot_index) > -1) {
+            _text = '<span class="select select_to view">' + _text + '</span>';
+        }
+        else {
+            _text = '<span class="select select_middle view">' + _text + '</span>';
+        }
+        return _text;
+    };
+	
+    var _focus_single_text = function (_index, _text) {
+        if ($.inArray(_index, _focus_head_index) > -1) {
+            _text = '<span class="select select_from select_to view">' + _text + '</span>';
+        }
+        return _text;
+    };
+    
+    var _selectable_text_word = this._selectable_text_word;
+    
+    var _ellipsis = '<span class="ellipsis">...</span>';
+    var _last_id = _selectable_text_word.word_count;
+    
+    for (var _i = 0; _i < _scope_coll.length(); _i++) {
+        var _sentence = '';
+        
+        var _scope = _scope_coll.get(_i);
+        var _from = _scope.get_from();
+        var _to = _scope.get_to();
+        
+        for (var _j = _from; _j < _to + 1; _j++) {
+            var _index = _j;
+            var _word = _selectable_text_word.get_word_by_index(_index);
+            var _text = _word.text();
+            
+            if (_j < _to
+                && _selectable_text_word.is_word_next_span(_word)) {
+                _text = _text + ' ';
+            }
+            else if ($.is_ascii(_text.substr(_text.length-1, 1))) {
+                _text = _text + ' ';
+            }
+            
+            for (var _k in _focus_index) {
+                if ($.inArray(_j, _focus_index[_k]) > -1) {
+                    if (_focus_index.length > 1) {
+                        _text = _focus_text(_j, _text);
+                    }
+                    else {
+                        _text = _focus_single_text(_j, _text);
+                    }
+                }    
+            }
+            
+            _sentence = _sentence + _text; 
+        }
+        
+        _sentence = $.trim(_sentence);
+        
+        //不同範圍之間，以空格斷句！
+        if (_from > 0) {
+            _sentence = _ellipsis + _sentence;
+        }
+        if (_i === _scope_coll.length() - 1 && _to < _last_id) {
+            _sentence = _sentence + _ellipsis;
+        }
+           
+        //var _sentence_span = $('<span></span>')
+        //    .html(_sentence)
+        //    .appendTo(_anchor_text);
+        $('<span></span>')
+            .html(_sentence)
+            .appendTo(_anchor_text);
+    }
+    
+    return _anchor_text;
+};
+
+/* End of file Selectable_text_anchor */
+/* Location: ./system/application/views/web_apps/Selectable_text_anchor.js */
---
+++ system/application/views/web_apps/selectable_text/Selectable_text_sentence.js
@@ -0,0 +1,262 @@
+/**
+ * Selectable_text_sentence
+ *
+ * @package     KALS
+ * @category    Webpage Application Libraries
+ * @author      Pulipuli Chen <pulipuli.chen@gmail.com>
+ * @copyright   Copyright (c) Expression year is undefined on line 7, column 33 in Templates/KALS/KALS_JavaScript_Class.js., Pudding Chen
+ * @license     http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link        https://github.com/pulipulichen/kals
+ * @version     1.0 2014/1/2 下午 09:17:54
+ */
+
+/**
+ * @memberOf {Selectable_text_sentence}
+ * @extends {KALS_user_interface}
+ * @constructor
+ * @param {Selectable_text} _selectable_text 父物件
+ */
+function Selectable_text_sentence(_selectable_text) {
+    
+    this._selectable_text = _selectable_text;
+    this._text = _selectable_text._text;
+    return this;
+}
+
+/**
+ * Extends from KALS_user_interface.
+ * @memberOf {Selectable_text_sentence}
+ */
+Selectable_text_sentence.prototype = new KALS_user_interface();
+
+/**
+ * 父物件
+ * @type {Selectable_text}
+ */
+Selectable_text_sentence.prototype._selectable_text;
+
+/**
+ * 主要可以選擇的物件
+ * @type {jQuery}
+ */
+Selectable_text_sentence.prototype._text;
+
+// -----------------------------------
+// 內部參數設定
+// -----------------------------------
+
+/**
+ * 標點符號的classname
+ * @type {String}
+ */
+Selectable_text_sentence.prototype.punctuation_classname = 'kals-punctuation';
+
+/**
+ * 分句用的標點符號classname
+ * @type {String}
+ */
+Selectable_text_sentence.prototype.sententce_punctuation_classname = 'kals-sentence-punctuation';
+
+/**
+ * 分句用索引的classname
+ * @type {String}
+ */
+Selectable_text_sentence.prototype.sententce_index_classname = 'kals-sentence-index';
+
+/**
+ * 句子特徵，保存句子切割的特徵。
+ * @type {Array|number}
+ */
+Selectable_text_sentence.prototype.sentence_structure = [];
+
+
+// -----------------------------------
+
+/**
+ * 取得句子位置的索引
+ * 
+ * 用來分析標註所在句子，跟段落paragraph是不一樣的。
+ * 布丁測試用
+ * 
+ * 2243 轉接完成
+ * 2246 檢查完成
+ * 
+ * @author Pudding Chen 20121228
+ * @return {Array}
+ */
+Selectable_text_sentence.prototype.get_sentence_index = function () {
+    
+    var _selectable_text_word = this._selectable_text.word;
+    
+    //如果已經作過這樣的分析的話
+    if (this._text.find('.'+this.sententce_index_classname).length > 0)
+    {
+            var _sentences = this._text.find('.'+this.sententce_index_classname);
+            var _sentence_index = [];
+            for (var _i = 0; _i < _sentences.length; _i++)
+            {
+                    var _sentence = _sentences.eq(_i);
+                    var _word_id = _sentence.attr('id');
+                    _word_id = _word_id.substr(_selectable_text_word.word_id_prefix.length, _word_id);
+                    _word_id = parseInt(_word_id,10);
+                    _sentence_index.push(_word_id);
+            }
+
+            return _sentence_index;
+    }
+
+    //先來看被視為分句的標點符號位置
+    _sentence_index = [0];
+    var _sentence_punctuation =  $('.'+this.sententce_punctuation_classname);
+
+    for (var _s = 0; _s < _sentence_punctuation.length; _s++)
+    {
+            var _id = _sentence_punctuation.eq(_s).attr('id');
+            //kals_word_953
+            //_id = _id.substring(10, _id.length);
+            _id = $.get_prefixed_id(_id);
+            _sentence_index.push(parseInt(_id,10));	
+    }
+
+    //再來看段落的最後一個字
+    var _last_word = this._text.find('.'+_selectable_text_word.word_classname+':last');
+    var _last_paragraph = _last_word.parents("."+_selectable_text_word.paragraph_classname+":first");
+
+    //var _last_paragraph_classname = _last_paragraph.attr('className');
+    //var _paragraph_classname_header = this.paragraph_classname + ' ' + this.paragraph_id_prefix;  
+    //var _last_pid = _last_paragraph_classname.substring(_paragraph_classname_header.length , _last_paragraph_classname.length);
+    //_last_pid = parseInt(_last_pid);
+    var _last_pid = $.get_class_prefixed_id(_last_paragraph, this.paragraph_id_prefix);
+
+    for (_i = 0; _i < _last_pid+1; _i++) {
+        var _paragraph = $('.' + this.paragraph_id_prefix + _i + ":last");
+
+        if (_paragraph.length === 1) 
+        {
+            _last_word = _paragraph.find('.'
+                    + _selectable_text_word.word_classname 
+                    + '.tooltip-trigger:last:not(.' 
+                    + this.sententce_punctuation_classname+')');
+
+            if (_last_word.length > 0) {
+                _id = _last_word.attr('id');
+
+                //_id = _id.substring(this.word_id_prefix.length, _id.length);
+                _id = $.get_prefixed_id(_id);
+                _sentence_index.push(parseInt(_id,10));
+
+                if (isNaN(_id))
+                {
+                    _last_word.css('color', 'blue')
+                            .attr('title', _id);
+                }
+            }
+        }
+    }
+
+    //檢查測試結果用
+    for (_i = 0; _i < _sentence_index.length; _i++) {
+
+        var _sentense_index_word = $('#' 
+                + _selectable_text_word.word_id_prefix 
+                + _sentence_index[_i]);
+        _sentense_index_word.addClass(this.sententce_index_classname);
+
+        //檢測找到的字是否真的是分句點，平時不使用應關掉
+        //_sentense_index_word.css("color", "red");
+        //alert([$('#kals_word_' + _i).length, _i]);
+    }
+
+    //排序
+    //_sentence_index.sort(function (_a, _b) {
+    //	return (_a - _b);
+    //});
+
+    return _sentence_index;
+};
+
+/**
+ * 增加句子的結構
+ * @returns {Selectable_text_sentence.prototype}
+ */
+Selectable_text_sentence.prototype.add_structure = function () {
+    var _word_count = this._selectable_text.word.word_count;
+    this.sentence_structure.push(_word_count);
+    return this;
+};
+
+/**
+ * 增加句子的結構，但是是句子的最後一句
+ * @returns {Selectable_text_sentence.prototype}
+ */
+Selectable_text_sentence.prototype.add_structure_last_word = function () {
+    var _word_count = this._selectable_text.word.word_count;
+    _word_count--;
+    
+    if (_word_count < 1) {
+        return this;
+    }
+    if (this.sentence_structure.length === 0) {
+        this.sentence_structure.push(_word_count);
+    }
+    else if (_word_count !== this.sentence_structure[this.sentence_structure.length-1]) {
+        this.sentence_structure.push(_word_count);
+    }
+    return this;
+};
+
+/**
+ * 取得目前計算的句子數量
+ * @returns {number}
+ */
+Selectable_text_sentence.prototype.count_strucutre = function () {
+    return this.sentence_structure.length;
+};
+
+/**
+ * 取得句子的結構
+ * @returns {Selectable_text_sentence.prototype}
+ */
+Selectable_text_sentence.prototype.get_structure = function () {
+    return this.sentence_structure;
+};
+
+// -------------------
+
+/**
+ * 儲存到快取中
+ * @param {String} _cache_id
+ * @param {funciton} _callback
+ * @returns {Selectable_text_sentence}
+ */
+Selectable_text_sentence.prototype.cache_save = function (_cache_id, _callback) {
+    _cache_id = _cache_id + '_sentence';
+    var _json = $.json_encode(this.sentence_structure);
+    //$.test_msg('sentence save: ' + _cache_id, _json);
+    //$.localStorage.set(_cache_id, _json);
+    KALS_context.storage.set(_cache_id, _json, _callback)
+    return this;
+};
+
+/**
+ * 從快取中復原
+ * @param {String} _cache_id
+ * @param {funciton} _callback
+ * @returns {Selectable_text_sentence}
+ */
+Selectable_text_sentence.prototype.cache_restore = function (_cache_id, _callback) {
+    _cache_id = _cache_id + '_sentence';
+    //var _json = $.localStorage.get(_cache_id);
+    //$.test_msg('sentence restore: ' + _cache_id, _json);
+    //this.sentence_structure = $.json_decode(_json);
+    
+    var _this = this;
+    KALS_context.storage.get(_cache_id, function (_value) {
+        _this.sentence_structure = $.json_decode(_value);
+        $.trigger_callback(_callback);
+    });
+    return this;
+};
+
+/* End of file Selectable_text_sentence */
+/* Location: ./system/application/views/web_apps/Selectable_text_sentence.js */
