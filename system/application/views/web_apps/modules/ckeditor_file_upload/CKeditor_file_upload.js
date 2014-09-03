cc1d6c0ded077ee012382fe2ff222ebccbca81ee
Pulipuli Chen <pulipuli.chen@gmail.com>
2014-09-03 13:55:25 +0800

    0903 修整程式碼

--- system/application/views/web_apps/modules/ckeditor_file_upload/CKeditor_file_upload.js
+++ system/application/views/web_apps/modules/ckeditor_file_upload/CKeditor_file_upload.js
@@ -53,6 +53,7 @@ if (typeof(KALS_CONFIG.modules.CKeditor_file_upload.enable) === "boolean"
 
 /**
  * 設定檔案上傳功能
+ * @version 20140902 Pulipuli Chen
  */
 CKeditor_file_upload.php_file_host = function () {
     
@@ -64,93 +65,9 @@ CKeditor_file_upload.php_file_host = function () {
         return;
     }
     
-    //$.test_msg(_btn.length);
-    
-    //window.CKEDITOR.tools.callFunction(1, '/ckfinder/userfiles/files/app.png', '');
-    
-//    if (typeof(window.CKEDITOR) === "object"
-//            && typeof(window.CKEDITOR.php_file_host) !== "function") {
-//        window.CKEDITOR.php_file_host = function () {
-//            //$.test_msg("rewe");
-//            this.tools.callFunction(1, '/ckfinder/userfiles/files/app.png', '');
-//        };
-//    }
-//    
-//    var myDefinition = window.CKEDITOR.tools.extend( {}, elementDefinition );
-//    var onClick = myDefinition.onClick;
-//    
-    //$.test_msg("rewe");
-//    //var target = elementDefinition[ 'for' ];		// [ pageId, elementId ]
-//    if ( !onClick || onClick.call( this, evt ) !== false )
-//    {
-//            //dialog.getContentElement( target[0], target[1] ).submit();
-//            this.disable();
-//    }
-
-    // 1. 把表單放上去
     var _php_file_host_upload_url = KALS_CONFIG.modules.CKeditor_file_upload.upload_url;
     var _php_file_host_get_link_url = KALS_CONFIG.modules.CKeditor_file_upload.get_link_url;
-    /*
-    var _form_html = '<form action="' + _php_file_host_upload_url + '" method="post" enctype="multipart/form-data" style="display:none;"><input class="fileupload" type="file" name="file"><input name="local_upload" type="hidden" value="1" />' 
-            //+ '<div id="progress" class="progress"><div class="progress-bar progress-bar-warning"></div></div>'
-            //+ '<button type="submit">遞交</button>'
-            + '</form>';
     
-    var _form = $(_form_html);
-    
-    //$.test_msg(_form_html);
-    
-    //_btn.css("border", "4px solid red");
-    
-    
-    _btn.after(_form);
-    
-    var _file_input = _form.find(".fileupload");
-    
-    
-    //_form.submit(function () {
-    var _upload = function () {
-        var _config = {
-            url: _php_file_host_upload_url,
-            get_link_url: _php_file_host_get_link_url,
-            userfile: _file_input,
-            callback: function (_data) {
-                //$.test_msg("form submit callback", _data);
-                window.CKEDITOR.tools.callFunction(1, _data, '');
-            }
-        };
-        //$.test_msg("form, submit", _option);
-        KALS_util.ajax_upload(_config);
-        //return false;
-    //});
-    };
-    
-    
-    _file_input.change(_upload);
-    _file_input.click();
-    
-    //setTimeout(function () {
-    //    _form.submit();
-    //}, 100);
-    
-    // 2. jQuery File Uploader化
-    // 最小化安裝 https://github.com/blueimp/jQuery-File-Upload/wiki/Basic-plugin
-    
-//    _form.fileupload({
-//        dataType: 'json',
-//        done: function (_e, _data) {
-//            $.test_msg("fileupload done", _data);
-//            //$.each(_data.result.files, function (_index, _file) {
-//            //    $.test_msg("ok?", _file.name);
-//            //});
-//        }
-//    });
-    
-    // 3. 點下選擇檔案的按鈕
-    
-    //window.CKEDITOR.tools.callFunction(1, '/ckfinder/userfiles/files/app.png', '');
-    //window.OnUploadCompleted(1, '/ckfinder/userfiles/files/app.png', '');
-    */
     var _config = {
         url: _php_file_host_upload_url,
         get_link_url: _php_file_host_get_link_url,
