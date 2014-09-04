e5717c2584091d35ebded9adac5f2df530cdb9da
Pulipuli Chen <pulipuli.chen@gmail.com>
2014-09-05 01:52:37 +0800

    0905 最小化 & 修正一個錯誤

---
+++ libraries/ckeditor/plugins/recordmp3js/lang/en.src.js
@@ -0,0 +1,15 @@
+/**
+ * 支持英文的语言包(文件名称en.js)，第一个参数是插件名称
+ */
+CKEDITOR.plugins.setLang('recordmp3js', 'en', {
+	tbTip    : 'Voice Recorder',
+	audio_link    : 'Voice Audio URL',
+        record    : 'Record',
+	dlgTitle : 'Voice Recorder',
+        sec: 'sec(s)',
+        remain: 'Remain: ',
+        recorded: 'Recorded: ',
+        mic_disabled: 'You have not enable microphone. Please allow browser to use microphone first.',
+        download_msg: 'Because of no upload server at your configuration, please download your record file by clicking download icon.'
+});
+ 
---
+++ libraries/ckeditor/plugins/recordmp3js/lang/zh.src.js
@@ -0,0 +1,12 @@
+CKEDITOR.plugins.setLang('recordmp3js', 'zh', {
+	tbTip    : '錄音機',
+	audio_link    : '錄音檔案連結',
+        record    : '錄音',
+	dlgTitle : '錄音機',
+        sec: '秒',
+        remain: '剩餘',
+        recorded: '已錄',
+        mic_disabled: '未偵測到麥克風，請允許瀏覽器啟用麥克風',
+        download_msg: '由於未設定檔案上傳伺服器，錄音結果不會插入到編輯器，請按下載圖示下載檔案。',
+        wait_message: '檔案處理中，請稍候'
+});
