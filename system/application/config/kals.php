ae80b3fc123dcecf3d31f2ae829d052b2ad29e51
Pulipuli Chen <pulipuli.chen@gmail.com>
2014-12-09 20:03:17 +0800

    1209 強制使用快取

--- system/application/config/kals.php
+++ system/application/config/kals.php
@@ -19,10 +19,6 @@
 // --------
 $config['crypt_salt'] = 'kals2010';
 
-$config['output.cache.enable'] = true; //是否使用快取功能，預設是true;
-$config['output.cache.expiration'] = 5;  //快取的單位是「分鐘」
-$config['output.package.enable'] = true; //是否啟用壓縮功能，預設是true;
-
 $config['CACHEABLE_TYPES'] = array('Domain', 'Webpage', 'Annotation', 'User', 'Group', 'Annotation_scope', 'Scope_anchor_text', 'Annotation_like'
         , 'Annotation_feature_collection', 'Annotation_like_collection', 'Annotation_respond_collection', 'Annotation_topic_respond_collection'
         , 'Annotation_scope_collection', 'Annotation_score_collection'
--- update_kals.sh
+++ update_kals.sh
@@ -1,8 +1,12 @@
-BRANCH=reading-map
+BRANCH=pudding
+KALS_PATH=/opt/lampp/htdocs/kals
 
-cd /opt/lampp/htdocs/kals
+cd $KALS_PATH
 git --git-dir=/opt/lampp/htdocs/kals/.git clean -f -d
 git --git-dir=/opt/lampp/htdocs/kals/.git reset --hard origin/$BRANCH
-git --git-dir=/opt/lampp/htdocs/kals/.git pull
+git --git-dir=/opt/lampp/htdocs/kals/.git pull --rebase --force kals
 
 git --git-dir=/opt/lampp/htdocs/kals/.git merge origin/$BRANCH --no-commit
+
+mv $KALS_PATH/system/application/config/kals_cache_package.php $KALS_PATH/system/application/config/kals_cache_package.old.php
+mv $KALS_PATH/system/application/config/kals_cache_package_enable.php $KALS_PATH/system/application/config/kals_cache_package.php
