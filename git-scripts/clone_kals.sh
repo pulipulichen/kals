1c8eba5d6224f76175e83c67b5924b3ea2bb8a4e
Pulipuli Chen <pulipuli.chen@gmail.com>
2015-06-16 21:22:27 +0800

    0616 加入clone的腳本

---
+++ git-scripts/clone_kals.sh
@@ -0,0 +1,7 @@
+if [ -z "$KALS_BRANCH" ]; then KALS_BRANCH=kals/master; fi
+if [ -z "$KALS_PATH" ]; then KALS_PATH=/var/www/moodle/kals; fi
+
+#echo $KALS_PATH
+
+cd $KALS_PATH
+git clone git://github.com/pulipulichen/kals.git
